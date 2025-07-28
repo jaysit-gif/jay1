0b5e74f091f341a59b600b399b77abcba30d5906
D J <jayadithya.dhurwey@gmail.com>
2025-07-28 14:44:07 +0530

    new sound

--- sound.c
+++ sound.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <windows.h>
+#include <conio.h>
 
-int main(void){
-    int i = 0;
-    while (i<5)
-    {
-        Beep(750,300);
-        Beep(1000,500);
-        i++;
+int main(void) {
+    printf("Press 'q' to stop the alarm!\n");
+
+    while (1) {
+        if (_kbhit()) {
+            char c = _getch();
+            if (c == 'q' || c == 'Q') {
+                printf("\nAlarm stopped.\n");
+                break;
+            }
+        }
+        Beep(750, 300);
+        Beep(1000, 500);
     }
     return 0;
 }
