d9d093a678a11060fde6522835c10faf868dbee6
D J <jayadithya.dhurwey@gmail.com>
2025-07-31 14:15:46 +0530

    sound added

--- crm.c
+++ crm.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <windows.h>
 
 int main(void){
     
     char *strings[4] = {"PHYSICS","MATHS","CHEMISTRY","PROGRAMMING"};
     for(int i = 0;i<4;i++){
         printf("I LOVE %s\n",strings[i]);
+        Beep(750,600);
     }
     return 0;
 }
